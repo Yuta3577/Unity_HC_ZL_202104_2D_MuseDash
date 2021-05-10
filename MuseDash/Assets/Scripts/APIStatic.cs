445acef37a8620996901e2fe996d0d88ca28323e
KID <50666207+KID421@users.noreply.github.com>
2021-05-10 20:11:16 +0800

    認識靜態 API

---
+++ MuseDash/Assets/Scripts/APIStatic.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class APIStatic : MonoBehaviour
+{
+    private void Start()
+    {
+        // 使用靜態 API
+        // 屬性
+        // 可以取得 Get、可以設定 Set
+        // 如果看到 Read Only 只能取得
+
+        // 1. 取得靜態的屬性
+        // 語法：類別.靜態屬性
+        print("隨機：" + Random.value);
+
+        // 2. 設定靜態的屬性
+        // 語法：類別.靜態屬性 指定 值；
+        Cursor.visible = false;
+
+        // 方法
+        // 3. 使用靜態的方法
+        // 語法：類別.靜態方法(對應的參數)；
+        int r = Random.Range(50, 150);
+        print("隨機攻擊力：" + r);
+    }
+}

237a4d60f9aacf85e9f5a3681ceb8635b1129a6f
KID <50666207+KID421@users.noreply.github.com>
2021-06-07 21:38:24 +0800

    添加音樂遊戲管理器與音樂資料

---
+++ MuseDash/Assets/Scripts/MusicData.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+// ScriptableObject 腳本化物件
+// 將資料製作成物件並保存在 Assets 內
+
+// CreateAssetMenu 腳本化物件類別的屬性
+// 設定選單的名稱以及生成物件的名稱
+[CreateAssetMenu(menuName = "KID/音樂資料", fileName = "音樂遊戲資料檔")]
+public class MusicData : ScriptableObject
+{
+    [Header("音樂")]
+    public AudioClip music;
+    [Header("音樂前面等待時間"), Range(0, 10)]
+    public float timeWait = 2f;
+    [Header("節點間隔時間"), Range(0, 5)]
+    public float interval = 1f;
+    [Header("節點移動速度"), Range(0, 1000)]
+    public float speed = 300;
+
+    // 陣列：儲存多筆相同類型的資料 - Array
+    // 語法：類型[]
+    // 陣列會有 Size 代表陣列資料的數量
+    [Header("音樂節點")]
+    public PointType[] points;
+}
+
+// 列舉 enum：下拉式選單 - 只能選取一個
+/// <summary>
+/// 音樂節點的類型：無節點、上方節點、下方節點、上下節點
+/// </summary>
+public enum PointType
+{
+    none, up, down, both
+}
---
+++ MuseDash/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    [Header("生成物件：上方")]
+    public GameObject objUp;
+    [Header("生成物件：下方")]
+    public GameObject objDown;
+    [Header("生成位置：上方")]
+    public Transform pointUp;
+    [Header("生成位置：下方")]
+    public Transform pointDown;
+    [Header("此關卡音樂資料")]
+    public MusicData musicData;
+
+    /// <summary>
+    /// 音源 - 音源物件
+    /// </summary>
+    private AudioSource aud;
+
+    private void Start()
+    {
+        aud = GameObject.Find("音源物件").GetComponent<AudioSource>();   // 尋找並取得音源元件
+        aud.clip = musicData.music;                                     // 指定音樂
+        aud.Play();                                                     // 播放音樂
+
+        Invoke("StartMusic", musicData.timeWait);       // 等待後開始生成
+    }
+
+    /// <summary>
+    /// 開始音樂節點生成
+    /// </summary>
+    private void StartMusic()
+    {
+        print("開始生成");
+
+        // Object.Instantiate(objUp);   // 原本寫法
+        Instantiate(objUp);             // 簡寫 - 省略 Object：生成(物件)
+    }
+}
